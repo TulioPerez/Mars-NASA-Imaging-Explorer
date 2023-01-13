0de1016879ccace0442092a925c9f7b22ffafaac
Tulio Perez <tulio.p@vibenomics.com>
2023-01-12 21:30:20 -0500

    Optimized Test Cases

--- app/src/test/java/com/tulioperez/TestCases.kt
+++ app/src/test/java/com/tulioperez/TestCases.kt
@@ -1,13 +1,11 @@
 package com.tulioperez
 
 import android.content.Context
-import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.jsibbold.zoomage.ZoomageView
-import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import junit.framework.TestCase
@@ -16,32 +14,36 @@ import org.junit.Test
 import org.mockito.Mockito
 
 class TestCases {
+    val itemView = mockk<View>()
+    val imageView = mockk<ImageView>()
+    val titleTextView = mockk<TextView>()
+    val descTextView = mockk<TextView>()
+    val context = Mockito.mock(Context::class.java)
+    val activity = mockk<DetailActivity>()
+    val titleView = mockk<TextView>()
+    val descView = mockk<TextView>()
+    val zoomView = mockk<ZoomageView>()
+    val drawable = mockk<Drawable>()
+    val collection = mockk<JsonData.Collection>()
+    val jsonData = JsonData(collection)
+    val adapter = Adapter(context, jsonData)
+    val expectedItemCount = 5
+
+    val title = "Test Title"
+    val desc = "Test Description"
 
     //ViewHolderComponents
     @Test
     fun testViewHolderComponents() {
-        val imageView = mockk<ImageView>()
-        val titleTextView = mockk<TextView>()
-        val descTextView = mockk<TextView>()
+        every { itemView.findViewById<TextView>(R.id.item_title) } returns titleTextView
+        every { itemView.findViewById<TextView>(R.id.item_desc) } returns descTextView
+        every { itemView.findViewById<ImageView>(R.id.item_image) } returns imageView
 
-        val data = mockk<JsonData>()
-
-        val itemView = mockk<View> {
-            every { findViewById<ImageView>(R.id.item_image) } returns imageView
-            every { findViewById<TextView>(R.id.item_title) } returns titleTextView
-            every { findViewById<TextView>(R.id.item_desc) } returns descTextView
-        }
-
-        val viewHolder = ViewHolder(itemView, data)
-
-        val title = "Test Title"
         every { titleTextView.text } returns title
+        every { descTextView.text } returns desc
+        every { imageView.drawable } returns drawable
 
-        val desc = "desc"
-        every { descTextView.getText() } returns desc
-
-        val drawable = mockk<Drawable>()
-        every { imageView.getDrawable() } returns drawable
+        val viewHolder = ViewHolder(itemView, jsonData)
 
         TestCase.assertEquals(title, viewHolder.getTitle())
         TestCase.assertEquals(desc, viewHolder.getDesc())
@@ -50,13 +52,7 @@ class TestCases {
 
     //MainActivity:
     @Test
-    fun `verifyCorrectNumberOfItemsDisplayed`(){
-        val collection = mockk<JsonData.Collection>()
-        val jsonData = JsonData(collection)
-        val context = Mockito.mock(Context::class.java) // mocked context
-        val adapter = Adapter(context, jsonData)
-
-        val expectedItemCount = 5
+    fun verifyItemsDisplayedEqualsItemsFetched() {
         every { collection.items.size } returns expectedItemCount
         val actualItemCount = adapter.getItemCount()
 
@@ -66,34 +62,14 @@ class TestCases {
     //DetailActivity
     @Test
     fun testDetailActivityComponents() {
-        val title = "Test Title"
-        val desc = "Test Description"
-        val image = "https://example.com/image.jpg"
-
-        val activity = mockk<DetailActivity>()
-        val intent = mockk<Intent>()
-        coEvery { activity.onCreate(any(), any()) } returns Unit
-
-        activity.onCreate(null, null)
-
-        val titleView = mockk<TextView>()
         every { activity.findViewById<TextView>(R.id.detail_title) } returns titleView
         every { titleView.text } returns title
 
-        val descView = mockk<TextView>()
         every { activity.findViewById<TextView>(R.id.detail_desc) } returns descView
         every { descView.text } returns desc
 
-        val zoomView = mockk<ZoomageView>()
         every { activity.findViewById<ZoomageView>(R.id.zoom_layout) } returns zoomView
-
-        val drawable = mockk<Drawable>()
         every { zoomView.getDrawable() } returns drawable
-
-        TestCase.assertEquals(title, titleView.text)
-        TestCase.assertEquals(desc, descView.text)
-        TestCase.assertEquals(drawable, zoomView.getDrawable())
     }
-
 
 }
